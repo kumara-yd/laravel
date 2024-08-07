5d36f4db0fecb12b12d392add0ada41c709f018a
Mas Yay <62861787+kumara-yd@users.noreply.github.com>
2024-08-07 13:15:55 +0700

    Update 2024_02_28_062640_add_softdelete_user.php

--- src/database/migrations/2024_02_28_062640_add_softdelete_user.php
+++ src/database/migrations/2024_02_28_062640_add_softdelete_user.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->string('profileImage')->nullable();
             $table->softDeletes();
         });
     }
@@ -23,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('deleted_at');
+            $table->dropColumn('profileImage');
         });
     }
 };
